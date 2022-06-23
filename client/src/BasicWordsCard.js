77df230a42c13cd889e7157cc37f702060a54ff8
Alexia Velador <alexiavel01@gmail.com>
2022-06-23 12:20:33 -0600

    fixed basic words card

--- client/src/BasicWordsCard.js
+++ client/src/BasicWordsCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SignCard.css'
 import { Button } from "./styles";
+import styled from "styled-components";
 
 function BasicWordsCard({sign}){
     
@@ -23,7 +24,7 @@ function BasicWordsCard({sign}){
                             <div className="back">
                                 <div id="inCard">
                                     {sign.basic_word === true ? <p className="desc">{sign.description}</p>: null}
-                                    <Button>Add To Favorites</Button>
+                                    <ButtonFave>Add To Favorites</ButtonFave>
                                     <p className="click">Click to View Example</p>
                                 </div>
                             </div>
@@ -35,6 +36,18 @@ function BasicWordsCard({sign}){
     )
 }
 
+const ButtonFave = styled.button`
+background-color: transparent;
+color: white;
+background-color: black;
+padding: 8px 16px;
+cursor: pointer;
+font-size: 20px;
+border-radius: 6px;
 
+&:hover {
+    background-color: rgba(31, 31, 31, 1);
+  }
+`;
 
 export default BasicWordsCard;
